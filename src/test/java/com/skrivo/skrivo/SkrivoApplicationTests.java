abd12e788049c5f695586fcdfeefdd23994c23cb
joakimai <joakimai@kth.se>
2022-02-05 21:56:23 +0100

    feat: Initializes a Spring Boot application with Spring Boot DevTools, Spring Web, Spring Data MongoDB and Embedded MongoDB Database.

---
+++ src/test/java/com/skrivo/skrivo/SkrivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skrivo.skrivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkrivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
