47694350d42743604509c2bc4c3096b633bc1412
edvinno <edvinno@kth.se>
2022-02-16 15:34:16 +0100

    feat: Add method to print Hello World to web page

--- src/main/java/com/skrivo/skrivo/SkrivoApplication.java
+++ src/main/java/com/skrivo/skrivo/SkrivoApplication.java
@@ -2,12 +2,20 @@ package com.skrivo.skrivo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SkrivoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkrivoApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello(){
+		return "Hello World!";
 	}
 
 }
