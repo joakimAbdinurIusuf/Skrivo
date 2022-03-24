d8fafd99e3eed26fe941815241207c84b49a3f50
wilhelmoberg <81524743+wilhelmoberg@users.noreply.github.com>
2022-03-24 16:51:26 +0100

    New template to work on
    * fix layout and folders
    
    * add README.md

---
+++ src/main/frontend/src/components/Dashboard.js
@@ -0,0 +1,44 @@
+import "./Dashboard.css";
+import { Button, Col, Row } from "react-bootstrap";
+import NetworkGraph from "./NetworkGraph";
+
+function Dashboard() {
+  return (
+    <div className={"Outer-border"}>
+      <div className="sidenav">
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+      </div>
+
+      <div className="content">
+        <Row>
+          <Col>
+            <Col className={"Navigation-buttons-column"}>
+              <Button size={"lg"} className={"Navigation-button"}>
+                Network
+              </Button>
+              <Button size={"lg"} className={"Navigation-button"}>
+                Completed tasks
+              </Button>
+              <Button size={"lg"} className={"Navigation-button"}>
+                Notifications
+              </Button>
+              <Button size={"lg"} className={"Navigation-button"}>
+                Reports
+              </Button>
+            </Col>
+            <Col className={"Network-graph"}>
+              <h1>Network Graph</h1>
+              <NetworkGraph />
+            </Col>
+          </Col>
+          <Col xl={4} className={"Word-cloud"}>
+            <h1>Word Cloud</h1>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+}
+export default Dashboard;
---
+++ src/main/frontend/src/components/NetworkGraph.js
@@ -0,0 +1,172 @@
+import React, { useState } from "react";
+import ReactFlow, { addEdge } from "react-flow-renderer";
+import "./network.css";
+
+const initialNodes = [
+  {
+    id: "1",
+    type: "input",
+    data: { label: "center node" },
+    position: { x: 500, y: 300 },
+    style: {
+      width: 64,
+      height: 64,
+      borderRadius: 64,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
+  {
+    id: "2",
+    type: "input",
+    data: { label: "node 2" },
+    position: { x: 600, y: 450 },
+    style: {
+      width: 160,
+      height: 160,
+      borderRadius: 160,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
+  {
+    id: "3",
+    type: "input",
+    data: { label: "node 3" },
+    position: { x: 300, y: 400 },
+    style: {
+      width: 90,
+      height: 90,
+      borderRadius: 90,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
+  {
+    id: "4",
+    type: "input",
+    data: { label: "node 4" },
+    position: { x: 300, y: 100 },
+    style: {
+      width: 90,
+      height: 90,
+      borderRadius: 90,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
+  {
+    id: "5",
+    type: "input",
+    data: { label: "node 5" },
+    position: { x: 40, y: 250 },
+    style: {
+      width: 140,
+      height: 140,
+      borderRadius: 140,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
+  {
+    id: "6",
+    type: "input",
+    data: { label: "node 6" },
+    position: { x: 800, y: 300 },
+    style: {
+      width: 75,
+      height: 75,
+      borderRadius: 75,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
+  {
+    id: "7",
+    type: "input",
+    data: { label: "node 7" },
+    position: { x: 650, y: 100 },
+    style: {
+      width: 190,
+      height: 190,
+      borderRadius: 190,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
+  {
+    id: "8",
+    type: "input",
+    data: { label: "node 8" },
+    position: { x: 750, y: 600 },
+    style: {
+      width: 55,
+      height: 55,
+      borderRadius: 55,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
+  {
+    id: "9",
+    type: "input",
+    data: { label: "node 9" },
+    position: { x: 20, y: 50 },
+    style: {
+      width: 120,
+      height: 120,
+      borderRadius: 120,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
+  {
+    id: "10",
+    type: "input",
+    data: { label: "node 10" },
+    position: { x: 200, y: 600 },
+    style: {
+      width: 135,
+      height: 135,
+      borderRadius: 135,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  },
+  { id: "e1-2", source: "1", target: "2" },
+  { id: "e1-3", source: "1", target: "3" },
+  { id: "e1-4", source: "1", target: "4" },
+  { id: "e1-5", source: "1", target: "5" },
+  { id: "e1-6", source: "1", target: "6" },
+  { id: "e1-7", source: "1", target: "7" },
+  { id: "e1-8", source: "1", target: "8" },
+  { id: "e1-9", source: "1", target: "9" },
+  { id: "e1-10", source: "1", target: "10" },
+];
+
+const onElementClick = (event, element) => console.log("click", element);
+
+const NetworkGraph = () => {
+  return (
+    <div className="NetworkGraph">
+      <ReactFlow
+        style={{ width: "100%", height: "50vh" }}
+        elements={initialNodes}
+        nodesDraggable={false}
+        onElementClick={onElementClick}
+      />
+    </div>
+  );
+};
+
+export default NetworkGraph;
