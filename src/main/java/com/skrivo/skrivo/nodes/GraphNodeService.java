0daea440d2a7c716397858ca6a4ba37ebffb3546
Bazze <83212143+bazze23@users.noreply.github.com>
2022-03-29 18:04:14 +0200

    Add getNetworkUserList() method, parsing from JSON

--- src/main/java/com/skrivo/skrivo/nodes/GraphNodeService.java
+++ src/main/java/com/skrivo/skrivo/nodes/GraphNodeService.java
@@ -3,17 +3,31 @@ package com.skrivo.skrivo.nodes;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
 
 @Service
 public class GraphNodeService {
 
     @GetMapping
-    public List<GraphNode> getNodes(){
-        return List.of(
-                new GraphNode(130, 0.5, 3.6, 2.0),
-                new GraphNode(140, 0.1, 1.5, 2.4),
-                new GraphNode(150, 0.7, 3.9, 1.1)
-        );
+    public NetworkUserList getNetworkUserList(){
+        Gson gson = new Gson();
+        StringBuilder sb = new StringBuilder();
+        try {
+            Scanner input = new Scanner(new FileReader("src/main/mongodb/data/fake_network.json"));
+            while (input.hasNext()) {
+                sb.append(input.next());
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            System.err.println(e);
+        }
+        String JSONString = sb.toString();
+        NetworkUserList networkUserList = gson.fromJson(JSONString, NetworkUserList.class);
+        
+        return networkUserList;
     }
 }
