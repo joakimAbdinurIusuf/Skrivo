436ae5cbcfdef99b713bc86be149b364e5f5d429
joakimai <joakimai@kth.se>
2022-04-10 00:17:44 +0200

    Removed some javascript files that were used to see if we could display data on the frontend with axios.

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import UC from './components/UserComponent'
 
 function App() {
   return (
     <div className="App">
-      <UC />
     </div>
   );
 }
