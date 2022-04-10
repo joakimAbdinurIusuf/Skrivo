17653f72d7eb97ab17cbc83b7a37893f3266e4e5
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-04-10 10:23:04 +0200

    Removed unecessary import.

--- src/main/java/com/skrivo/skrivo/nodes/GraphNodeRepository.java
+++ src/main/java/com/skrivo/skrivo/nodes/GraphNodeRepository.java
@@ -2,8 +2,6 @@ package com.skrivo.skrivo.nodes;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface GraphNodeRepository extends MongoRepository<GraphNode, String> {
     GraphNode findGraphNodeById(String id);
 }
