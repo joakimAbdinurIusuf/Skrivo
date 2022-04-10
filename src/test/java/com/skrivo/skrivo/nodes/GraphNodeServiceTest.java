89c4616210adbffb2e2e180d8f03a3d158d227d5
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-04-10 12:25:16 +0200

    Added another test class.

---
+++ src/test/java/com/skrivo/skrivo/nodes/GraphNodeServiceTest.java
@@ -0,0 +1,16 @@
+package com.skrivo.skrivo.nodes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphNodeServiceTest {
+
+    @Test
+    void getAllNodes() {
+    }
+
+    @Test
+    void getWordsAndFrequenciesForOneNode() {
+    }
+}
