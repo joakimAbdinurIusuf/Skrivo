6be2901f5560f0532ebd45474ddf2054a99b1760
Wilhelm Öberg <woberg@ug.kth.se>
2022-04-13 14:55:19 +0200

    add test

--- src/main/frontend/src/components/Dashboard.test.js
+++ src/main/frontend/src/components/Dashboard.test.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Dashboard from "./Dashboard";
+import Sidebar from "./Sidebar";
 import {render} from "@testing-library/react";
+import Flow from "./Flow";
 
 
-test("Snapshot test to find text on dashboard", () => {
-    const {getByText} = render(<Dashboard/>);
+test("find network graph text", () => {
+    const {getByText} = render(<Flow/>);
     const linkElement = getByText(/center node/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test("find sidebar text", () => {
+    const {getByText} = render(<Sidebar/>);
+    const linkElement = getByText(/Messages/i);
     expect(linkElement).toBeInTheDocument();
 });
 
