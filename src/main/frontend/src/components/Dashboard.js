1212e878839619c4b79ae315a17a7cece490382e
Wilhelm Öberg <woberg@ug.kth.se>
2022-04-13 19:21:53 +0200

    Word on front

--- src/main/frontend/src/components/Dashboard.js
+++ src/main/frontend/src/components/Dashboard.js
@@ -1,10 +1,9 @@
 import { Button, Col, Row } from "react-bootstrap";
 import React from "react";
 import Flow from "./Flow";
-import wordcloud from "./images/Steve.png"
-import { Image } from "react-bootstrap";
 import Sidebar from "./Sidebar";
 import "./Dashboard.css";
+import WordCloud from "./WordCloud";
 
 class Dashboard extends React.Component {
   state = {};
@@ -34,8 +33,7 @@ class Dashboard extends React.Component {
               </Col>
             </Col>
             <Col xl={4} className={"Word-cloud"}>
-              <h1>Word Cloud</h1>
-              <Image src={wordcloud} fluid/>
+              <WordCloud/>
             </Col>
           </Row>
         </div>
--- src/main/frontend/src/components/Flow.js
+++ src/main/frontend/src/components/Flow.js
@@ -1,45 +1,27 @@
-import { color } from "@mui/system";
 import { useState } from "react";
 import ReactFlow , {Controls} from "react-flow-renderer";
 import "./Dashboard.css";
 
 const initialNodes = [
-  // {
-  //   id: "1",
-  //   type: "input",
-  //   data: { label: "Input Node" },
-  //   position: { x: 250, y: 25 },
-  // },
-
-  // {
-  //   id: "2",
-  //   // you can also pass a React component as a label
-  //   data: { label: <div>Default Node</div> },
-  //   position: { x: 100, y: 125 },
-  // },
-  // {
-  //   id: "3",
-  //   type: "output",
-  //   data: { label: "Output Node" },
-  //   position: { x: 250, y: 250 },
-  // },
-  {id:"1", type:"default", data:{label:"center node"}, position:{x:500,y:300},style: {
+  {id:"1", type:"input", data:{label:"center node"}, position:{x:500,y:300},style: {
 		width: 64,
 		height: 64,
 		borderRadius: 64,
+	  	fontSize: 15,
 		display: "flex",
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"2", type:"default", data:{label:"node 2"}, position:{x:600,y:450}, style: {
+	{id:"2", type:"output", data:{label:"node 2"}, position:{x:600,y:450}, style: {
 		width: 160,
 		height: 160,
 		borderRadius: 160,
+			fontSize: 40,
 		display: "flex",
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"3", type:"default", data:{label:"node 3"}, position:{x:300,y:400}, style: {
+	{id:"3", type:"output", data:{label:"node 3"}, position:{x:300,y:400}, style: {
 		width: 90,
 		height: 90,
 		borderRadius: 90,
@@ -47,7 +29,7 @@ const initialNodes = [
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"4", type:"default", data:{label:"node 4"}, position:{x:300,y:100}, style: {
+	{id:"4", type:"output", data:{label:"node 4"}, position:{x:300,y:100}, style: {
 		width: 90,
 		height: 90,
 		borderRadius: 90,
@@ -55,7 +37,7 @@ const initialNodes = [
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"5", type:"default", data:{label:"node 5"}, position:{x:40,y:250}, style: {
+	{id:"5", type:"output", data:{label:"node 5"}, position:{x:40,y:250}, style: {
 		width: 140,
 		height: 140,
 		borderRadius: 140,
@@ -63,7 +45,7 @@ const initialNodes = [
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"6", type:"default", data:{label:"node 6"}, position:{x:800,y:300}, style: {
+	{id:"6", type:"output", data:{label:"node 6"}, position:{x:800,y:300}, style: {
 		width: 75,
 		height: 75,
 		borderRadius: 75,
@@ -71,7 +53,7 @@ const initialNodes = [
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"7", type:"default", data:{label:"node 7"}, position:{x:650,y:100}, style: {
+	{id:"7", type:"output", data:{label:"node 7"}, position:{x:650,y:100}, style: {
 		width: 190,
 		height: 190,
 		borderRadius: 190,
@@ -79,7 +61,7 @@ const initialNodes = [
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"8", type:"default", data:{label:"node 8"}, position:{x:750,y:600}, style: {
+	{id:"8", type:"output", data:{label:"node 8"}, position:{x:750,y:600}, style: {
 		width: 55,
 		height: 55,
 		borderRadius: 55,
@@ -87,16 +69,15 @@ const initialNodes = [
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	  {id:"9", type:"default", data:{label:"node 9"}, position:{x:20,y:50}, style: {
+	  {id:"9", type:"output", data:{label:"node 9"}, position:{x:20,y:50}, style: {
 		width: 120,
 		height: 120,
 		borderRadius: 120,
 		display: "flex",
 		alignItems: "center",
-		justifyContent: "center",
-    background: "lightcoral",
+		justifyContent: "center"
 	  }},
-	{id:"10", type:"default", data:{label:"node 10"}, position:{x:200,y:600}, style: {
+	{id:"10", type:"output", data:{label:"node 10"}, position:{x:200,y:600}, style: {
 		width: 135,
 		height: 135,
 		borderRadius: 135,
@@ -105,25 +86,26 @@ const initialNodes = [
 		justifyContent: "center"
 	  }},
 ];
+const edgeStyle = {stroke:"black"}
 
 const initialEdges = [
-  { id: "e1-2", source: "1", target: "2" },
-  { id: "e1-3", source: "1", target: "3" },
-  { id: "e1-4", source: "1", target: "4" },
-  { id: "e1-5", source: "1", target: "5" },
-  { id: "e1-6", source: "1", target: "6" },
-  { id: "e1-7", source: "1", target: "7" },
-  { id: "e1-8", source: "1", target: "8" },
-  { id: "e1-9", source: "1", target: "9" },
-  { id: "e1-10", source: "1", target: "10" },
+  { id: "e1-2", source: "1", target: "2", type: 'straight', style: edgeStyle },
+  { id: "e1-3", source: "1", target: "3", type: 'straight', style: edgeStyle },
+  { id: "e1-4", source: "1", target: "4", type: 'straight', style: edgeStyle },
+  { id: "e1-5", source: "1", target: "5", type: 'straight', style: edgeStyle },
+  { id: "e1-6", source: "1", target: "6", type: 'straight', style: edgeStyle },
+  { id: "e1-7", source: "1", target: "7", type: 'straight', style: edgeStyle },
+  { id: "e1-8", source: "1", target: "8", type: 'straight', style: edgeStyle },
+  { id: "e1-9", source: "1", target: "9", type: 'straight', style: edgeStyle },
+  { id: "e1-10", source: "1", target: "10", type: 'straight', style: edgeStyle },
 ];
+
 
 function Flow() {
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
 
-  return <ReactFlow nodes={nodes} edges={edges}>
-    <Controls />
+  return <ReactFlow nodes={nodes} edges={edges} nodesDraggable={true}>
   </ReactFlow>;
 }
 
