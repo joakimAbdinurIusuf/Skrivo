5439e9e0531f59f4be76a2dc3dfe60d9433d8b25
wilhelmoberg <81524743+wilhelmoberg@users.noreply.github.com>
2022-04-25 15:56:54 +0200

    Wordcloud (#83)
    * Word on front
    
    * work on wordcloud
    
    Co-authored-by: Wilhelm Öberg <woberg@ug.kth.se>

--- src/main/frontend/src/components/Dashboard.test.js
+++ src/main/frontend/src/components/Dashboard.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Sidebar from "./Sidebar";
-import {render} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import Flow from "./Flow";
 
 
--- src/main/frontend/src/components/Flow.js
+++ src/main/frontend/src/components/Flow.js
@@ -1,45 +1,217 @@
-import { color } from "@mui/system";
-import { useState } from "react";
-import ReactFlow , {Controls} from "react-flow-renderer";
+import React, { useState } from "react";
+import ReactFlow , {useNodesState,
+	useEdgesState,Controls} from "react-flow-renderer";
 import "./Dashboard.css";
 
 const initialNodes = [
-  // {
-  //   id: "1",
-  //   type: "input",
-  //   data: { label: "Input Node" },
-  //   position: { x: 250, y: 25 },
-  // },
-
-  // {
-  //   id: "2",
-  //   // you can also pass a React component as a label
-  //   data: { label: <div>Default Node</div> },
-  //   position: { x: 100, y: 125 },
-  // },
-  // {
-  //   id: "3",
-  //   type: "output",
-  //   data: { label: "Output Node" },
-  //   position: { x: 250, y: 250 },
-  // },
-  {id:"1", type:"default", data:{label:"center node"}, position:{x:500,y:300},style: {
+  {id:"1", type:"input", data:{label:"center node",words: [
+			  {
+				  text: "In",
+				  value: "64"
+			  },
+			  {
+				  text: "Duis",
+				  value: "78"
+			  },
+			  {
+				  text: "lorem,",
+				  value: "32"
+			  },
+			  {
+				  text: "interdum",
+				  value: "50"
+			  },
+			  {
+				  text: "Quisque",
+				  value: "98"
+			  },
+			  {
+				  text: "arcu",
+				  value: "73"
+			  },
+			  {
+				  text: "convallis",
+				  value: "46"
+			  },
+			  {
+				  text: "Suspendisse",
+				  value: "18"
+			  },
+			  {
+				  text: "molestie",
+				  value: "91"
+			  },
+			  {
+				  text: "Aliquam",
+				  value: "73"
+			  },
+			  {
+				  text: "lectus",
+				  value: "41"
+			  },
+			  {
+				  text: "quis",
+				  value: "22"
+			  },
+			  {
+				  text: "est",
+				  value: "37"
+			  },
+			  {
+				  text: "ullamcorper",
+				  value: "71"
+			  },
+			  {
+				  text: "arcu",
+				  value: "37"
+			  },
+			  {
+				  text: "quis,",
+				  value: "10"
+			  },
+			  {
+				  text: "Nullam",
+				  value: "66"
+			  },
+			  {
+				  text: "luctus",
+				  value: "88"
+			  },
+			  {
+				  text: "vulputate",
+				  value: "31"
+			  },
+			  {
+				  text: "eros.",
+				  value: "26"
+			  },
+			  {
+				  text: "porttitor",
+				  value: "76"
+			  },
+			  {
+				  text: "molestie",
+				  value: "15"
+			  },
+			  {
+				  text: "massa.",
+				  value: "64"
+			  },
+			  {
+				  text: "dui.",
+				  value: "25"
+			  },
+			  {
+				  text: "nec,",
+				  value: "66"
+			  },
+			  {
+				  text: "eleifend",
+				  value: "92"
+			  },
+			  {
+				  text: "eget",
+				  value: "65"
+			  },
+			  {
+				  text: "sit",
+				  value: "25"
+			  },
+			  {
+				  text: "natoque",
+				  value: "42"
+			  },
+			  {
+				  text: "consequat,",
+				  value: "19"
+			  },
+			  {
+				  text: "turpis",
+				  value: "60"
+			  },
+			  {
+				  text: "Sed",
+				  value: "33"
+			  },
+			  {
+				  text: "amet",
+				  value: "92"
+			  },
+			  {
+				  text: "sapien,",
+				  value: "67"
+			  },
+			  {
+				  text: "nec",
+				  value: "46"
+			  },
+			  {
+				  text: "augue",
+				  value: "92"
+			  },
+			  {
+				  text: "convallis",
+				  value: "79"
+			  },
+			  {
+				  text: "mauris,",
+				  value: "31"
+			  },
+			  {
+				  text: "adipiscing.",
+				  value: "32"
+			  },
+			  {
+				  text: "nunc,",
+				  value: "88"
+			  },
+			  {
+				  text: "tempor",
+				  value: "68"
+			  },
+			  {
+				  text: "in,",
+				  value: "14"
+			  },
+			  {
+				  text: "mi",
+				  value: "32"
+			  },
+			  {
+				  text: "dignissim",
+				  value: "86"
+			  },
+			  {
+				  text: "natoque",
+				  value: "93"
+			  },
+			  {
+				  text: "egestas",
+				  value: "80"
+			  },
+			  {
+				  text: "sociis",
+				  value: "32"
+			  }
+		  ]}, position:{x:500,y:300},style: {
 		width: 64,
 		height: 64,
 		borderRadius: 64,
+	  	fontSize: 15,
 		display: "flex",
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"2", type:"default", data:{label:"node 2"}, position:{x:600,y:450}, style: {
+	{id:"2", type:"output", data:{label:"node 2"}, position:{x:600,y:450}, style: {
 		width: 160,
 		height: 160,
 		borderRadius: 160,
+			fontSize: 40,
 		display: "flex",
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"3", type:"default", data:{label:"node 3"}, position:{x:300,y:400}, style: {
+	{id:"3", type:"output", data:{label:"node 3"}, position:{x:300,y:400}, style: {
 		width: 90,
 		height: 90,
 		borderRadius: 90,
@@ -47,7 +219,7 @@ const initialNodes = [
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"4", type:"default", data:{label:"node 4"}, position:{x:300,y:100}, style: {
+	{id:"4", type:"output", data:{label:"node 4"}, position:{x:300,y:100}, style: {
 		width: 90,
 		height: 90,
 		borderRadius: 90,
@@ -55,7 +227,7 @@ const initialNodes = [
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"5", type:"default", data:{label:"node 5"}, position:{x:40,y:250}, style: {
+	{id:"5", type:"output", data:{label:"node 5"}, position:{x:40,y:250}, style: {
 		width: 140,
 		height: 140,
 		borderRadius: 140,
@@ -63,7 +235,7 @@ const initialNodes = [
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"6", type:"default", data:{label:"node 6"}, position:{x:800,y:300}, style: {
+	{id:"6", type:"output", data:{label:"node 6"}, position:{x:800,y:300}, style: {
 		width: 75,
 		height: 75,
 		borderRadius: 75,
@@ -71,7 +243,7 @@ const initialNodes = [
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"7", type:"default", data:{label:"node 7"}, position:{x:650,y:100}, style: {
+	{id:"7", type:"output", data:{label:"node 7"}, position:{x:650,y:100}, style: {
 		width: 190,
 		height: 190,
 		borderRadius: 190,
@@ -79,7 +251,7 @@ const initialNodes = [
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	{id:"8", type:"default", data:{label:"node 8"}, position:{x:750,y:600}, style: {
+	{id:"8", type:"output", data:{label:"node 8"}, position:{x:750,y:600}, style: {
 		width: 55,
 		height: 55,
 		borderRadius: 55,
@@ -87,16 +259,15 @@ const initialNodes = [
 		alignItems: "center",
 		justifyContent: "center"
 	  }},
-	  {id:"9", type:"default", data:{label:"node 9"}, position:{x:20,y:50}, style: {
+	  {id:"9", type:"output", data:{label:"node 9"}, position:{x:20,y:50}, style: {
 		width: 120,
 		height: 120,
 		borderRadius: 120,
 		display: "flex",
 		alignItems: "center",
-		justifyContent: "center",
-    background: "lightcoral",
+		justifyContent: "center"
 	  }},
-	{id:"10", type:"default", data:{label:"node 10"}, position:{x:200,y:600}, style: {
+	{id:"10", type:"output", data:{label:"node 10"}, position:{x:200,y:600}, style: {
 		width: 135,
 		height: 135,
 		borderRadius: 135,
@@ -105,26 +276,38 @@ const initialNodes = [
 		justifyContent: "center"
 	  }},
 ];
+const edgeStyle = {stroke:"black"}
 
 const initialEdges = [
-  { id: "e1-2", source: "1", target: "2" },
-  { id: "e1-3", source: "1", target: "3" },
-  { id: "e1-4", source: "1", target: "4" },
-  { id: "e1-5", source: "1", target: "5" },
-  { id: "e1-6", source: "1", target: "6" },
-  { id: "e1-7", source: "1", target: "7" },
-  { id: "e1-8", source: "1", target: "8" },
-  { id: "e1-9", source: "1", target: "9" },
-  { id: "e1-10", source: "1", target: "10" },
+  { id: "e1-2", source: "1", target: "2", type: 'straight', style: edgeStyle },
+  { id: "e1-3", source: "1", target: "3", type: 'straight', style: edgeStyle },
+  { id: "e1-4", source: "1", target: "4", type: 'straight', style: edgeStyle },
+  { id: "e1-5", source: "1", target: "5", type: 'straight', style: edgeStyle },
+  { id: "e1-6", source: "1", target: "6", type: 'straight', style: edgeStyle },
+  { id: "e1-7", source: "1", target: "7", type: 'straight', style: edgeStyle },
+  { id: "e1-8", source: "1", target: "8", type: 'straight', style: edgeStyle },
+  { id: "e1-9", source: "1", target: "9", type: 'straight', style: edgeStyle },
+  { id: "e1-10", source: "1", target: "10", type: 'straight', style: edgeStyle },
 ];
 
-function Flow() {
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+const onNodeClick = (event, node) => console.log('click node', node);
 
-  return <ReactFlow nodes={nodes} edges={edges}>
-    <Controls />
-  </ReactFlow>;
-}
+const Flow = () => {
+	const [nodes] = useNodesState(initialNodes);
+	const [edges] = useEdgesState(initialEdges);
+
+	const [captureElementClick] = useState(true);
+
+	return (
+		<ReactFlow
+			nodes={nodes}
+			edges={edges}
+			onNodeClick={captureElementClick ? onNodeClick : undefined}
+			fitView
+		>
+			<Controls/>
+		</ReactFlow>
+	);
+};
 
 export default Flow;
