9f37f28b95acc7e9cf513184c455054f4209fff3
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-04-27 14:57:08 +0200

    Add more nodes (#88)
    * Changed install instructions in readme.
    
    * Errors might drop this solution.
    
    * Managed to change the format of the database so resources are easier to manipulate in React.
    
    * Changed size of one node.
    
    * more nodes

--- src/main/java/com/skrivo/skrivo/SkrivoApplication.java
+++ src/main/java/com/skrivo/skrivo/SkrivoApplication.java
@@ -16,8 +16,12 @@ import java.util.Random;
 public class SkrivoApplication {
 
 	Random random = new Random();
-	double rangeMin = 0.0;
-	double rangeMax = 2*Math.PI;
+	double rangeMinAngle = 0.0;
+	double rangeMaxAngle = 2*Math.PI;
+	double rangeMinSize = 0.1;
+	double rangeMaxSize = 0.5;
+	double rangeMinDistance = 1.0;
+	double rangeMaxDistance = 5.0;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkrivoApplication.class, args);
@@ -36,7 +40,7 @@ public class SkrivoApplication {
 			GraphNode centerNode = new GraphNode(
 					0.3,
 					0,
-					rangeMin + (rangeMax - rangeMin) * random.nextDouble(),
+					rangeMinAngle + (rangeMaxAngle - rangeMinAngle) * random.nextDouble(),
 					List.of(createJSONObject("React", "10"),
 							createJSONObject("Java", "3"),
 							createJSONObject("Python", "1"),
@@ -47,14 +51,24 @@ public class SkrivoApplication {
 							createJSONObject("Backend", "4"),
 							createJSONObject("Frontend", "1"),
 							createJSONObject("Databases", "1")
+							createJSONObject("Angular", "10"),
+							createJSONObject("Express", "3"),
+							createJSONObject("Node", "1"),
+							createJSONObject("Spring Boot", "7"),
+							createJSONObject("Flask", "1"),
+							createJSONObject("C++", "1"),
+							createJSONObject("Go", "2"),
+							createJSONObject("Lisp", "4"),
+							createJSONObject("Haskell", "1"),
+							createJSONObject("MongoDB", "1")
 					)
 			);
 			graphNodeRepository.insert(centerNode);
 
 			GraphNode graphNode1 = new GraphNode(
-					0.36311161543,
-					0.222,
-					rangeMin + (rangeMax - rangeMin) * random.nextDouble(),
+					rangeMinSize + (rangeMaxSize - rangeMinSize) * random.nextDouble(),
+					rangeMinDistance + (rangeMaxDistance - rangeMinDistance) * random.nextDouble(),
+					rangeMinAngle + (rangeMaxAngle - rangeMinAngle) * random.nextDouble(),
 					List.of(createJSONObject("KTH", "10"),
 							createJSONObject("Liu", "3"),
 							createJSONObject("LTH", "1"),
@@ -65,9 +79,47 @@ public class SkrivoApplication {
 							createJSONObject("UCLA", "4"),
 							createJSONObject("Cambridge", "1"),
 							createJSONObject("Oxford", "1")
+							createJSONObject("SU", "10"),
+							createJSONObject("Lancaster", "3"),
+							createJSONObject("Manchester University", "1"),
+							createJSONObject("UoA", "7"),
+							createJSONObject("", "1"),
+							createJSONObject("Harvard", "1"),
+							createJSONObject("Stanfords state", "2"),
+							createJSONObject("UCLAMLA", "4"),
+							createJSONObject("Tambridge", "1"),
+							createJSONObject("Uxfordser", "1")
 					)
 			);
 			graphNodeRepository.insert(graphNode1);
+
+			GraphNode graphNode2 = new GraphNode(
+					rangeMinSize + (rangeMaxSize - rangeMinSize) * random.nextDouble(),
+					rangeMinDistance + (rangeMaxDistance - rangeMinDistance) * random.nextDouble(),
+					rangeMinAngle + (rangeMaxAngle - rangeMinAngle) * random.nextDouble(),
+					List.of(createJSONObject("YouTube", "10"),
+							createJSONObject("Medium", "3"),
+							createJSONObject("Wordpress", "1"),
+							createJSONObject("Substack", "7"),
+							createJSONObject("Blogging", "1"),
+							createJSONObject("Vlogging", "1"),
+							createJSONObject("Books", "2"),
+							createJSONObject("Backend", "4"),
+							createJSONObject("Frontend", "1"),
+							createJSONObject("Databases", "1")
+							createJSONObject("Angular", "10"),
+							createJSONObject("Express", "3"),
+							createJSONObject("Node", "1"),
+							createJSONObject("Spring Boot", "7"),
+							createJSONObject("Flask", "1"),
+							createJSONObject("C++", "1"),
+							createJSONObject("Go", "2"),
+							createJSONObject("Lisp", "4"),
+							createJSONObject("Haskell", "1"),
+							createJSONObject("MongoDB", "1")
+					)
+			);
+			graphNodeRepository.insert(graphNode2);
 		};
 	}
 }
