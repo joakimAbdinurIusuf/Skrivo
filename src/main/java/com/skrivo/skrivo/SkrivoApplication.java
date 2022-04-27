d638e467bbca98d37f6770c44a83f9d4910dc34f
Joakim Abdinur Iusuf <92917053+joakimAbdinurIusuf@users.noreply.github.com>
2022-04-27 12:20:30 +0200

    Errors might drop this solution.

--- src/main/java/com/skrivo/skrivo/SkrivoApplication.java
+++ src/main/java/com/skrivo/skrivo/SkrivoApplication.java
@@ -2,14 +2,15 @@ package com.skrivo.skrivo;
 
 import com.skrivo.skrivo.nodes.GraphNode;
 import com.skrivo.skrivo.nodes.GraphNodeRepository;
+import org.json.JSONObject;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -28,114 +29,15 @@ public class SkrivoApplication {
 	CommandLineRunner commandLineRunner(GraphNodeRepository graphNodeRepository, MongoTemplate mongoTemplate) {
 		return args -> {
 			GraphNode graphNode1 = new GraphNode(
-					0.31993293378583665,
-					3.11830276223588,
+					0.3,
+					0,
 					rangeMin + (rangeMax - rangeMin) * random.nextDouble(),
-					List.of("kicked",
-			        "la",
-					"suicide",
-					"gosh",
-					"mmmm",
-					"ganas",
-					"tla",
-					"goods",
-					"cruel",
-					"press",
-					"nutella",
-					"throw",
-					"albeit",
-					"fra",
-					"layout",
-					"recorded",
-					"barbs",
-					"screwed",
-					"elgato",
-					"titas",
-					"rizal",
-					"bf",
-					"fever",
-					"inca",
-					"llama",
-					"truck",
-					"lurgy",
-					"progress",
-					"stage",
-					"unagi",
-					"laine",
-					"plz",
-					"lansi",
-					"cornwall",
-					"marathon",
-					"catching",
-					"politics",
-					"abusive",
-					"leh",
-					"nathan",
-					"agonising",
-					"bless",
-					"buying",
-					"hug",
-					"soul",
-					"wig",
-					"tbf",
-					"mv",
-					"plisss",
-					"cunt"
-					),
-					List.of(3,
-					10,
-					2,
-					6,
-					1,
-					1,
-					1,
-					1,
-					1,
-					1,
-					1,
-					2,
-					1,
-					4,
-					2,
-					1,
-					1,
-					2,
-					1,
-					1,
-					1,
-					2,
-					7,
-					1,
-					1,
-					2,
-					1,
-					1,
-					14,
-					1,
-					1,
-					3,
-					1,
-					1,
-					3,
-					1,
-					1,
-					1,
-					1,
-					1,
-					1,
-					2,
-					1,
-					15,
-					5,
-					1,
-					1,
-					1,
-					1,
-					1
+					//
 					)
 			);
 			graphNodeRepository.insert(graphNode1);
 
+			/*
 			GraphNode graphNode2 = new GraphNode(
 					0.3425687126900313,
 					2.54715549340008,
@@ -571,6 +473,7 @@ public class SkrivoApplication {
 					)
 			);
 			graphNodeRepository.insert(graphNode5);
+			 */
 		};
 	}
 }
--- src/main/java/com/skrivo/skrivo/nodes/GraphNode.java
+++ src/main/java/com/skrivo/skrivo/nodes/GraphNode.java
@@ -2,11 +2,11 @@ package com.skrivo.skrivo.nodes;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.json.JSONObject;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * @author Edvin Nordling, Joakim Abdinur Iusuf
@@ -20,66 +20,56 @@ public class GraphNode {
     private double size;
     private double x;
     private double y;
-    private List<String> words;
-    private List<Integer> wordFrequency;
+    private String[][] wordsAndFrequencies;
 
     /**
      * Constructor without nodeID
      */
-    public GraphNode(double size, double distance, double angle, List<String> words, List<Integer> wordFrequency) {
+    public GraphNode(double size, double distance, double angle, String[][] wordsAndFrequencies) {
         this.size = size;
         x = Math.cos(angle) * distance * 100;
         y = Math.sin(angle) * distance * 100;
-        this.words = words;
-        this.wordFrequency = wordFrequency;
+        this.wordsAndFrequencies = wordsAndFrequencies;
     }
 
     public String getId() {
         return id;
     }
 
-    public double getSize() {
-        return size;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public List<String> getWords() {
-        return words;
-    }
-
-    public List<Integer> getWordFrequency() {
-        return wordFrequency;
-    }
-
     public void setId(String id) {
         this.id = id;
+    }
+
+    public double getSize() {
+        return size;
     }
 
     public void setSize(double size) {
         this.size = size;
     }
 
+    public double getX() {
+        return x;
+    }
+
     public void setX(double x) {
         this.x = x;
+    }
+
+    public double getY() {
+        return y;
     }
 
     public void setY(double y) {
         this.y = y;
     }
 
-    public void setWords(List<String> words) {
-        this.words = words;
+    public String[][] getWordsAndFrequencies() {
+        return wordsAndFrequencies;
     }
 
-    public void setWordFrequency(List<Integer> wordFrequency) {
-        this.wordFrequency = wordFrequency;
+    public void setWordsAndFrequencies(String[][] wordsAndFrequencies) {
+        this.wordsAndFrequencies = wordsAndFrequencies;
     }
 
     @Override
@@ -87,12 +77,14 @@ public class GraphNode {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GraphNode graphNode = (GraphNode) o;
-        return Double.compare(graphNode.size, size) == 0 && Double.compare(graphNode.x, x) == 0 && Double.compare(graphNode.y, y) == 0 && Objects.equals(id, graphNode.id) && Objects.equals(words, graphNode.words) && Objects.equals(wordFrequency, graphNode.wordFrequency);
+        return Double.compare(graphNode.size, size) == 0 && Double.compare(graphNode.x, x) == 0 && Double.compare(graphNode.y, y) == 0 && Objects.equals(id, graphNode.id) && Arrays.equals(wordsAndFrequencies, graphNode.wordsAndFrequencies);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, size, x, y, words, wordFrequency);
+        int result = Objects.hash(id, size, x, y);
+        result = 31 * result + Arrays.hashCode(wordsAndFrequencies);
+        return result;
     }
 
     @Override
@@ -102,8 +94,7 @@ public class GraphNode {
                 ", size=" + size +
                 ", x=" + x +
                 ", y=" + y +
-                ", words=" + words +
-                ", wordFrequency=" + wordFrequency +
+                ", wordsAndFrequencies=" + Arrays.toString(wordsAndFrequencies) +
                 '}';
     }
 }
